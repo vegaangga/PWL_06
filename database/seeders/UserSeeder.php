7bce4da376fc008b52475fd26a0af6352d82430f
vegaangga <vegaangga27@gmail.com>
2021-03-16 15:44:17 +0700

    Prak 2 Langkah 3 - Mengedit file UserSeeders untu memasukkan data awal user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,6 +13,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password')
+        ]);
     }
 }
